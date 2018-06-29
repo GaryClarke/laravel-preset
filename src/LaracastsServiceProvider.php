d4e52b765c7cd24f324ea797af0f7d6b61e8b97b
Gary Clarke <garyclarke1974@gmail.com>
2018-06-29 14:06:03 +0100

    First draft

---
+++ src/LaracastsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Laracasts\LaravelPreset;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Console\PresetCommand;
+
+class LaracastsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        PresetCommand::macro('laracasts', function ($command) {
+
+            Preset::install();
+        });
+    }
+}
